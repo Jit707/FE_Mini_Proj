bb0c1b706f0edbb868811c00fe593e2ba67182a7
Jit <jit.oung254343@gmail.com>
2025-01-09 00:19:03 +0700

    finished Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,43 @@
 
 import './App.css'
+import logoa1 from './assets/image/logoa 1.png'
+import vector from './assets/image/Vector.png'
+import headerPic from './assets/image/Objects.png'
+import viewDemo from './assets/image/Group 111.png'
 
 function App() {
   
 
   return (
     <>
-      <div className='bg-blue-100 w-screen p-0 m-0'>Logo</div>
+      <div className='bg-background w-screen py-8 flex flex-row justify-evenly gap-64 border-b-2 border-gray'>
+        <div>
+          <img className='w-36'src={logoa1} alt="Logoa1" />
+        </div>
+        <div>
+          <img className='w-5' src={vector} alt="vector" />
+          </div>
+          
+      </div>
+      <div className='bg-background'>
+        <img src={headerPic} alt="A man waring purple shirt" />
+      </div>
+
+      <div className='bg-background flex flex-col items-center '>
+        <div className='bg-white text-l p-3 rounded-md text-purple font-bold'>Welcome QuickSpace</div>
+        <div className='text-4xl text-primary font-bold py-2 pt-7 '>Satisfied Customer</div>
+        <div className='text-4xl text-primary font-bold py-2'>Is the Best Business</div>
+        <div className='text-4xl text-primary font-bold py-2'>Strategy</div>
+        <div className='text-lg py-4 w-2/3 text-gray'>Backed with vast experience and fully up to date skills set, at Enuke Software offer the best in class E-Prescription solution
+        </div>
+        <button 
+        className='bg-button text-white py-4 px-20 font-semibold rounded-md'>Try Free
+        </button>
+        <div>
+          <img className='w-28 pt-7' src={viewDemo} alt="Click to view Demo" />
+        </div>
+
+      </div>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      'primary':'hsla(235, 79%, 17%, 1)',
+    'background': '#E4ECFF',
+    'purple' : 'hsla(232, 67%, 48%, 0.8)',
+    'gray':'hsla(236, 12%, 44%, 1)',
+    'white':'hsl(212 100% 100%)',
+    'button':'hsla(232, 67%, 48%, 1)'
+      ,
+    },
     extend: {},
   },
   plugins: [],
